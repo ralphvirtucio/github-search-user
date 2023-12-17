a7027dd9d8b8f3129d163a94cc31a402b7e4abd0
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-12-17 11:33:26 +0800

    Feat: Application Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+// api to be use - https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-a-user
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,26 +23,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div>
+      <h1>Hello World</h1>
+     </div>
     </>
   )
 }
